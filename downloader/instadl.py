06cb951d4c54d101a043cab8f0d19ff3e20d176d
Sunny Shankar <sunny.shankar44@gmail.com>
2020-12-18 14:39:20 +0530

    changed-to-rapid-api

--- downloader/instadl.py
+++ downloader/instadl.py
@@ -1,22 +1,22 @@
 import requests
-import json
-header_data = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
-}
+
+url = "https://instagram40.p.rapidapi.com/account-info"
 
 
-def get_insta_user_data(usrname):
-    url = f"https://www.instagram.com/{usrname.lower()}/?__a=1"
-    res = requests.get(url, headers=header_data)
-    r = json.loads(res.content)
-    insta_user_data = {
-        'full_name': r['graphql']['user']['full_name'],
-        'follower': r['graphql']['user']['edge_followed_by']['count'],
-        'following': r['graphql']['user']['edge_follow']['count'],
-        'bio': r['graphql']['user']['biography'],
-        'pic_url': r['graphql']['user']['profile_pic_url_hd'],
+def get_insta_user_data(username):
+    querystring = {"username": username}
+
+    headers = {
+        'x-rapidapi-key': "0c8a0f1269msh55c1b3929770a46p12c5cdjsn80e59a973e98",
+        'x-rapidapi-host': "instagram40.p.rapidapi.com"
     }
-    return insta_user_data
-
-
-print(get_insta_user_data('sunny._.shankar'))
+    r = requests.request(
+        "GET", url, headers=headers, params=querystring).json()
+    user_data = {
+        'full_name': r['full_name'],
+        'bio': r['biography'],
+        'following': r['edge_follow']['count'],
+        'follower': r['edge_followed_by']['count'],
+        'pic_url': r['profile_pic_url_hd']
+    }
+    return user_data
